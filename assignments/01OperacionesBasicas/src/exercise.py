43c5bf6cd5c284f7242e7ffefc65bdfd23897288
Alejandrocm10 <A01641391@itesm.mx>
2021-08-23 23:18:42 +0000

    envio1

--- assignments/01OperacionesBasicas/src/exercise.py
+++ assignments/01OperacionesBasicas/src/exercise.py
@@ -1,7 +1,7 @@
 def main():
     #escribe tu código abajo de esta línea
-    numero_1 = int(input("Ingresa un numero: "))
-    numero_2 = int(input("Ingresa un numero: "))
+    numero_1 = int(input("Dame un número: "))
+    numero_2 = int(input("Dame un número: "))
     suma = numero_1 + numero_2
     resta = numero_1 - numero_2
     multiplicación = numero_1 * numero_2
